939d8cf394e782429cacf5220f1156eabc6afa5c
zbilalozgen <zahidbilalozgen@gmail.com>
2019-11-12 19:22:51 +0300

    Project completed

--- src/Coin.js
+++ src/Coin.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import './Coin.css';
-import CoinTura from './1-lira-tura.png';
-import CoinYazi from './1-lira-yazi.png';
+import React from "react";
+import "./Coin.css";
+import CoinTura from "./1-lira-tura.png";
+import CoinYazi from "./1-lira-yazi.png";
 
-const Coin = (props) => {
+const Coin = props => {
   return (
     <div className="Coin-container">
-      <div className={`Coin ${props.donuyor ? 'Coin-rotate' : ''}`}>
-        <img src={CoinTura} className={(props.side === 'yazi' ? 'Coin-back' : 'Coin-front')} />
-        <img src={CoinYazi} className={(props.side === 'yazi' ? 'Coin-front' : 'Coin-back')} />
+      <div className={`Coin ${props.donuyor ? "Coin-rotate" : ""}`}>
+        {console.log(props)}
+        <img
+          src={CoinTura}
+          className={props.side === "yazi" ? "Coin-back" : "Coin-front"}
+        />
+        <img
+          src={CoinYazi}
+          className={props.side === "yazi" ? "Coin-front" : "Coin-back"}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Coin;
